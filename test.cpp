a98657bdc8848f49a6bec9c1fa640824d5ec4bc7
Rezy <64067988+RezyIGL@users.noreply.github.com>
2024-05-29 19:40:28 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,17 @@
 int main() {
+    int a = ..., b = ..., c = ...;
+    int n = ..., m = ...;
+    int x = 2 * (c + b);
+    int y = (6 + a) * (x - c);
+    int z = 0;
     int s = 0;
-    for (int i = 10; i > 0; --i) {
-        int x = s;
-        s += i;
+    for (int i = 0; i < n; i++) {
+        int x = c - 3;
+        y = a + c;
+        z = (a + x) * (y - 8);
+        for (int j = 0; j < m; j++) {
+            int x = (z + b) * (x + c);
+            s = s + b + c - x;
+        }
     }
 }
